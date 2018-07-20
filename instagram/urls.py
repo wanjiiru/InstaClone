93d408b064548d61e668c99ad32e9ef7d3daca22
wanjiiru <liz240@live.com>
2018-07-20 12:33:52 +0300

    add login and registration templates

--- instagram/urls.py
+++ instagram/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('insta.urls'))
+    url(r'', include('insta.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 
 ]
