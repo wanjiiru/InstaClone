be36691557373f52d07afcfc7141a430e85f791f
wanjiiru <liz240@live.com>
2018-07-20 14:36:30 +0300

    add image and profile models

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.forms import ImageField
+
 
 # Create your models here.
+
+
+
+class Profile(models.Model):
+    profile_pic = ImageField(blank=True, manual_crop = '1080x800')
+    bio = models.CharField(max_length=255,blank=True)
+    belongs_to = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+
+    def __str__(self):
+        return str(self.belongs_to)
+
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+
+class Image(models.Model):
+    pic = ImageField(blank=True,manual_crop='1080x800')
+    name= models.CharField(max_length=55)
+    caption = models.TextField(blank=True)
+    likes=models.BooleanField(default=False)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE())
+
+
--- insta/views.py
+++ insta/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Image, Profile
 
 
 # Create your views here.
 
 @login_required(login_url='/accounts/login/')
 def home(request):
-    return render(request, 'home.html', {})
+    return render(request,'home.html',{})
+
+
+@login_required(login_url='/login')
+def profile(request):
+    current_user=request.user
+    user_details=Profile.objects.filter(belongs_to = current_user.id).all()
+
+    return render(request,'profile/profile.html',{"user_details":user_details})
