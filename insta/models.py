33dff3d9ca2b808385f578cdfb474deea5aa30d4
wanjiiru <liz240@live.com>
2018-07-26 20:40:26 +0300

    add Like model

--- insta/models.py
+++ insta/models.py
@@ -40,7 +40,6 @@ class Image(models.Model):
     pic=ImageField(manual_crop='1080x800')
     name= models.CharField(max_length=55)
     caption = models.TextField(blank=True)
-    likes=models.BooleanField(default=False)
     profile= models.ForeignKey(User, on_delete=models.CASCADE)
     profile_details = models.ForeignKey(Profile)
 
@@ -78,6 +77,17 @@ class Comment(models.Model):
 
     def __str__(self):
         return str(self.comment)
+
+
+
+class Likes(models.Model):
+    liker=models.ForeignKey(User)
+    image =models.ForeignKey(Image)
+
+
+    def __str__(self):
+        return self.image
+
 
 
 
